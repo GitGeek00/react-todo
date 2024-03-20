d538a9711b3ba1a43678efb0f01e3232c182eb57
Maher Algepah <maher@mahersoft.onmicrosoft.com>
2024-03-19 22:58:48 -0700

    merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-var todoList = [
-  { id: 1, title: "Complete Assessment" },
-  { id: 2, title: "50% Completed Assessment" },
-  { id: 3, title: "Not Started" },
-];
+import React from "react";
+import "./style/App.css";
+import TodoList from "./components/TodoList";
+import AddTodoForm from "./components/AddTodoForm";
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [newTodo , setNewTodo] = React.useState('');
 
   return (
     <div className="todoList">
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
-    </>
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p className='yourInput'>{newTodo ? `Your input: |${newTodo}|` : 'Your input goes here'}</p>
+      <TodoList />
+    </div>
   );
 }
 
