18d1887c6175396d2126e9ea92dda50543088c89
Maher Algepah <maher@mahersoft.onmicrosoft.com>
2024-04-16 21:01:52 -0700

    lesson_1_7

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import "./style/App.css";
 import TodoList from "./components/TodoList";
 import AddTodoForm from "./components/AddTodoForm";
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = useState(
-    localStorage.getItem("savedTodoList")
-      ? JSON.parse(localStorage.getItem("savedTodoList"))
-      : []
-  );
+function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: localStorage.getItem("savedTodoList")
+              ? JSON.parse(localStorage.getItem("savedTodoList"))
+              : []
+          }
+        });
+      }, 2000)
+    });
+    promise.then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
   }, [todoList]);
-
-  return [todoList, setTodoList];
-};
-
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([newTodo, ...todoList]);
@@ -30,14 +42,34 @@ function App() {
 
   return (
     <>
-      <h1 className="welcome fade-out">
-        Welcome to My Todo List REACT Application
-      </h1>
-      <div className="todoList">
-        <h1>My Todo List</h1>
-        <AddTodoForm onAddTodo={addTodo} />
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-      </div>
+      {(isLoading)
+        ?
+        <>
+          <div class="sk-cube-grid">
+            <div class="sk-cube sk-cube1"></div>
+            <div class="sk-cube sk-cube2"></div>
+            <div class="sk-cube sk-cube3"></div>
+            <div class="sk-cube sk-cube4"></div>
+            <div class="sk-cube sk-cube5"></div>
+            <div class="sk-cube sk-cube6"></div>
+            <div class="sk-cube sk-cube7"></div>
+            <div class="sk-cube sk-cube8"></div>
+            <div class="sk-cube sk-cube9"></div>
+          </div>
+          <h1>Loading.....</h1>
+          <h1 className="welcome">
+            Welcome to My Todo List REACT Application
+          </h1>
+          <h1 className="welcome">Developer: Maher Algepah</h1>
+        </>
+        : <>
+          <div className="todoList">
+            <h1>My Todo List</h1>
+            <AddTodoForm onAddTodo={addTodo} />
+            <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+          </div>
+        </>
+      }
     </>
   );
 }
