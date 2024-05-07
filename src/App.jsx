9c79fb2275e7ca3c8e62a8c6e5730d5ed5633913
Maher Algepah <maher@mahersoft.onmicrosoft.com>
2024-05-07 12:33:21 -0700

    lesson_1_9

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, Fragment } from "react";
 import "./style/App.css";
 import TodoList from "./components/TodoList";
 import AddTodoForm from "./components/AddTodoForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
@@ -82,36 +83,45 @@ function App() {
   }
 
   return (
-    <>
-      {(isLoading)
-        ?
-        <>
-          <div className="sk-cube-grid">
-            <div className="sk-cube sk-cube1"></div>
-            <div className="sk-cube sk-cube2"></div>
-            <div className="sk-cube sk-cube3"></div>
-            <div className="sk-cube sk-cube4"></div>
-            <div className="sk-cube sk-cube5"></div>
-            <div className="sk-cube sk-cube6"></div>
-            <div className="sk-cube sk-cube7"></div>
-            <div className="sk-cube sk-cube8"></div>
-            <div className="sk-cube sk-cube9"></div>
-          </div>
-          <h1>Loading.....</h1>
-          <h1 className="welcome">
-            Welcome to My Todo List REACT Application
-          </h1>
-          <h1 className="welcome">Developer: Maher Algepah</h1>
-        </>
-        : <>
-          <div className="todoList">
-            <h1>My Todo List</h1>
-            <AddTodoForm onAddTodo={addTodo} />
-            <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-          </div>
-        </>
-      }
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element=
+        {(
+          <>
+            {(isLoading)
+              ?
+              <>
+                <div className="sk-cube-grid">
+                  <div className="sk-cube sk-cube1"></div>
+                  <div className="sk-cube sk-cube2"></div>
+                  <div className="sk-cube sk-cube3"></div>
+                  <div className="sk-cube sk-cube4"></div>
+                  <div className="sk-cube sk-cube5"></div>
+                  <div className="sk-cube sk-cube6"></div>
+                  <div className="sk-cube sk-cube7"></div>
+                  <div className="sk-cube sk-cube8"></div>
+                  <div className="sk-cube sk-cube9"></div>
+                </div>
+                <h1>Loading.....</h1>
+                <h1 className="welcome">
+                  Welcome to My Todo List REACT Application
+                </h1>
+                <h1 className="welcome">Developer: Maher Algepah</h1>
+              </>
+              :
+              <>
+                <div className="todoList">
+                  <h1>My Todo List</h1>
+                  <AddTodoForm onAddTodo={addTodo} />
+                  <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+                </div>
+              </>
+            }
+          </>
+        )} />
+        <Route path="/new" element={(<h1>New Todo List</h1>)} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
